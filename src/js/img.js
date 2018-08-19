99c8ad5ecd44d7363795cf6cb62894c8d7296cbb
Paola Guarasci <paolaguarasci@gmail.com>
2018-08-19 17:46:22 +0200

    first deploy on firebase hosting

--- src/js/img.js
+++
@@ -1,9 +0,0 @@
-import codeURL from "../img/code.png";
-
-// for (let i = 0; i < 3; i++) {
-//   let img = document.createElement("img");
-//   img.src = "dist/" + codeURL;
-//   img.style = "background: #2B3A42; padding: 20px";
-//   img.width = 128;
-//   document.body.appendChild(img);
-// }
